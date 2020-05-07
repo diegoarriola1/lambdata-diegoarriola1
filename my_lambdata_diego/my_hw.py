ccced7490365d3b86ff0c240a15233059e1066a7
Diego Arriola <diego@Diegos-MacBook-Pro.local>
2020-05-07 00:22:25 -0400

    updating git with example

--- my_lambdata_diego/my_hw.py
+++ my_lambdata_diego/my_hw.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 # new function from HW
 
@@ -12,3 +13,12 @@ def train_validate_test_split(df, train_percent=.6, validate_percent=.2, seed=No
     validate = df.iloc[perm[train_end:validate_end]]
     test = df.iloc[perm[validate_end:]]
     return train, validate, test
+
+# np.random.seed([3,1415])
+# df = pd.DataFrame(np.random.rand(10, 5), columns=list('ABCDE'))
+
+# train, validate, test = train_validate_test_split(df)
+
+# print(train)
+# print(validate)
+# print(test)
