43cc18bff41f1e0c3f9a5d07db9eb7027e4068dc
Diego Arriola <diego@Diegos-MacBook-Pro.local>
2020-05-06 23:41:14 -0400

    added new hw function for test

--- my_lambdata_diego/my_mod.py
+++ my_lambdata_diego/my_mod.py
@@ -1,7 +1,23 @@
 # my_script.py
 
+#enlarge function
+
 def enlarge(n):
     return n * 100
+
+# new function from HW
+
+def train_validate_test_split(df, train_percent=.6, validate_percent=.2, seed=None):
+    np.random.seed(seed)
+    perm = np.random.permutation(df.index)
+    m = len(df.index)
+    train_end = int(train_percent * m)
+    validate_end = int(validate_percent * m) + train_end
+    train = df.iloc[perm[:train_end]]
+    validate = df.iloc[perm[train_end:validate_end]]
+    test = df.iloc[perm[validate_end:]]
+    return train, validate, test
+
 
 if __name__ == "__main__":
     x = 5
