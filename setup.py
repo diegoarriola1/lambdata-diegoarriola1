46a75083548e9ccdf054c68805395377893fee73
Diego Arriola <diego@Diegos-MacBook-Pro.local>
2020-05-07 00:24:41 -0400

    new version upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="my-lambdata-diego", # the name that you will install via pip
-    version="1.1",
+    version="1.2",
     author="Diego Arriola",
     author_email="diegoarriola5@gmail.com",
     description="A short description",
