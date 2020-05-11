b66d0a524f9e3e15342b3f7cc8c441aad64285df
Diego Arriola <diego@Diegos-MacBook-Pro.local>
2020-05-10 21:39:20 -0400

    Polo class

---
+++ my_lambdata_diego/polos.py
@@ -0,0 +1,24 @@
+
+
+
+
+class Polo:
+    def __init__(self, size, color, price=69.99):
+        self.size = size
+        self.color = color
+        self.price = price
+
+    def wash(self, ):
+        print(f"WASHING THE {self.size.upper()} {self.color.upper()} POLO!")
+
+
+if __name__ == "__main__":
+
+
+    polo = Polo(size="Large", color="Blue")
+    print(polo.size, polo.color, polo.price)
+    polo.wash()
+
+    polo2 = Polo(size="Small", color="Yellow")
+    print(polo.size, polo.color, polo.price)
+    polo2.wash()
