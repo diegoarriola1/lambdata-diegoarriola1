343f04a08514312fb1d506b4ee1cd2301f885150
Diego Arriola <diego@Diegos-MacBook-Pro.local>
2020-05-10 23:03:54 -0400

    Second hw

--- my_lambdata_diego/second_hw.py
+++ my_lambdata_diego/second_hw.py
@@ -1,21 +1,38 @@
 
 from pandas import DataFrame
-# State abbreviation -> Full Name and visa versa. FL -> Florida, etc. 
+# State abbreviation -> Full Name and visa versa. FL -> Florida, etc.
 # (Handle Washington DC and territories like Puerto Rico etc.)
 
 def add_state_names(my_df):
     new_df = my_df.copy()
-    names_map = {"CA":"Cali", "CO":"Colo", "CT":"Conn"}
+    names_map = {"AL": "Alabama", "AK": "Alaska", "AZ": "Arizona",
+                 "AR": "Arkansas", "CA": "California", "CO": "Colorado",
+                 "CT": "Connecticut", "DE": "Delaware", "FL": "Florida",
+                 "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho",
+                 "IL": "Illinois", "IN": "Indiana", "IA": "Iowa",
+                 "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana",
+                 "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts",
+                 "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi",
+                 "MO": "Missouri", "MT": "Montana", "NE": "Nebraska",
+                 "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey",
+                 "NM": "New Mexico", "NY": "New York", "NC": "North Carolina",
+                 "ND": "North Dakota", "OH": "Ohio", "OK": "Oklahoma",
+                 "OR": "Oregon", "PA": "Pennsylvania", "RI": "Rhode Island",
+                 "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee",
+                 "TX": "Texas", "UT": "Utah", "VT": "Vermont",
+                 "VA": "Virginia", "WA": "Washington", "WV": "West Virginia",
+                 "WI": "Wisconsin", "WY": "Wyoming",}
+
     new_df["name"] = new_df["abbrev"].map(names_map)
     return new_df
-    
+
 
 
 
 if __name__ == "__main__":
-    
+
     df = DataFrame({"abbrev": ["CA", "CO", "CT","DC","TX"]})
-    print(df.head())
+    print(df)
 
     df2 = add_state_names(df)
-    print(df2.head())
+    print(df2)
