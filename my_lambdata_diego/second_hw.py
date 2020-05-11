1f05a661ac79c9a00160d53e1489e7fd6ef56dcd
Diego Arriola <diego@Diegos-MacBook-Pro.local>
2020-05-10 23:41:53 -0400

    Upload

--- my_lambdata_diego/second_hw.py
+++ my_lambdata_diego/second_hw.py
@@ -21,13 +21,34 @@ def add_state_names(my_df):
                  "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee",
                  "TX": "Texas", "UT": "Utah", "VT": "Vermont",
                  "VA": "Virginia", "WA": "Washington", "WV": "West Virginia",
-                 "WI": "Wisconsin", "WY": "Wyoming",}
+                 "WI": "Wisconsin", "WY": "Wyoming", "DC": "Washington D.C."}
 
     new_df["name"] = new_df["abbrev"].map(names_map)
     return new_df
 
+# def add_state_abbrev(my_df):
+#     new_df = my_df.copy
+#     names_map = {"AL": "Alabama", "AK": "Alaska", "AZ": "Arizona",
+#                  "AR": "Arkansas", "CA": "California", "CO": "Colorado",
+#                  "CT": "Connecticut", "DE": "Delaware", "FL": "Florida",
+#                  "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho",
+#                  "IL": "Illinois", "IN": "Indiana", "IA": "Iowa",
+#                  "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana",
+#                  "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts",
+#                  "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi",
+#                  "MO": "Missouri", "MT": "Montana", "NE": "Nebraska",
+#                  "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey",
+#                  "NM": "New Mexico", "NY": "New York", "NC": "North Carolina",
+#                  "ND": "North Dakota", "OH": "Ohio", "OK": "Oklahoma",
+#                  "OR": "Oregon", "PA": "Pennsylvania", "RI": "Rhode Island",
+#                  "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee",
+#                  "TX": "Texas", "UT": "Utah", "VT": "Vermont",
+#                  "VA": "Virginia", "WA": "Washington", "WV": "West Virginia",
+#                  "WI": "Wisconsin", "WY": "Wyoming", "DC": "Washington D.C."}
 
-
+#     for name in df["name"]:
+#         df["abbrev"] = df
+#     return new_df
 
 if __name__ == "__main__":
 
@@ -36,3 +57,11 @@ if __name__ == "__main__":
 
     df2 = add_state_names(df)
     print(df2)
+
+    # df = DataFrame({"name": ["California", "Colorado",
+    #                          "Connecticut", "Washington D.C.",
+    #                          "Texas"]})
+    # print(df)
+
+    # df2 = add_state_abbrev(df)
+    # print(df2)
