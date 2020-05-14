b91fec22af8661465ac76d4a4f73a129256d4dde
Diego Arriola <diego@Diegos-MacBook-Pro.local>
2020-05-13 23:53:41 -0400

    pytest adjustment

---
+++ test/pytest_assignment_test.py
@@ -0,0 +1,20 @@
+
+from pandas import DataFrame
+
+from my_lambdata.assignment import add_state_names
+
+# OBJECTIVE: test the add_state_names() function from the my_lambdata/assignment.py file
+
+
+def test_add_state_names(self):
+    # expect that it has one more column and the same number of rows, after we invoke the function
+    # expect that certain column names exist before and certain col names exist after
+
+    df = DataFrame({"abbrev":["CA","CO","CT","DC","TX"]})
+    assert (list(df.columns) == ['abbrev'])
+
+    result = add_state_names(df)
+
+    assert (list(result.columns) == ["abbrev", "name"])
+    assert (result.iloc[0]["abbrev"] == "CA")
+    assert (result.iloc[0]["name"] == "Cali")
